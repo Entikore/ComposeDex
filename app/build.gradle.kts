828fe680b7d7a24602d76816f6fb709e41e86a7b
Malte Grebe-Lüth <m.grebe-lueth@proton.me>
2026-06-23 10:49:20 +0200

    build: migrate to agp 9
    bumped dependencies:
      androidGradlePlugin 8.12.1 -> 9.2.1
      hilt 2.57 -> 2.59.2
      kotlin 2.2.10 -> 2.4.0
      ksp 2.2.10-2.0.2 -> 2.3.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.detekt)
     alias(libs.plugins.ksp)
@@ -43,19 +42,21 @@ android {
         compose = true
         buildConfig = true
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
-            freeCompilerArgs.add("-Xannotation-default-target=param-property")
-        }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+        freeCompilerArgs.add("-Xannotation-default-target=param-property")
     }
-    detekt {
-        config.setFrom("$rootDir/config/detekt.yml")
-        buildUponDefaultConfig = true
-        allRules = false
-        parallel = true
-        autoCorrect = true
-    }
+}
+
+detekt {
+    config.setFrom("$rootDir/config/detekt.yml")
+    buildUponDefaultConfig = true
+    allRules = false
+    parallel = true
+    autoCorrect = true
 }
 
 dependencies {
@@ -69,7 +70,6 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.google.fonts)
 
     implementation(libs.androidx.core.ktx)
@@ -81,6 +81,7 @@ dependencies {
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
+    ksp(libs.kotlin.metadata.jvm)
 
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.hilt.navigation)
@@ -130,4 +131,9 @@ dependencies {
     androidTestImplementation(libs.hilt.android.testing)
     kspAndroidTest(libs.hilt.android.comp)
     androidTestImplementation(project(":sharedTestCode"))
+}
+
+// https://docs.gradle.org/9.0.0-milestone-9/userguide/upgrading_version_8.html#test_task_fails_when_no_tests_are_discovered
+tasks.withType<AbstractTestTask>().configureEach {
+    failOnNoDiscoveredTests = false
 }
